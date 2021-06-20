9326d7f0e9a80aa96a143e27ec17023f698b0fc2
abdulhakim_altunkaya <altunkaya2@gmail.com>
2021-06-20 17:52:25 +0300

    fs 3

--- server.js
+++ server.js
@@ -5,7 +5,31 @@ const app = express();
 
 connectDB();
 
+app.set("view engine", "ejs");
+app.use(express.urlencoded{extended: true});
+app.use("/assets", express.static("static"));
 
+app.get("/", function(req, res){
+  res.render("index");
+});
+app.get("/coding-skills", function(req, res){
+  res.render("a1_coding");
+});
+app.get("/python-constructor", function(req, res){
+  res.render("a2_python");
+});
+app.get("/deploying-nodejs", function(req, res){
+  res.render("a3_nodejs");
+});
+app.get("/python-pie", function(req, res){
+  res.render("a4_pie");
+});
+app.get("/broken-image-nodejs", function(req, res){
+  res.render("a5_nodejs_image");
+});
+app.get("/ejs1", function(req, res){
+  res.render("ejs1");
+});
 app.get("/test1", function(req, res){
   res.send("It is very very very really working dude");
 });
