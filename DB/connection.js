aadea73182fb76d52aeb4b482e6f66c7bea7c67e
abdulhakim_altunkaya <altunkaya2@gmail.com>
2021-06-20 17:33:16 +0300

    Endevour

---
+++ DB/connection.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const url = "mongodb+srv://abdulhakim:seattle1@cluster0.j3afp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const connectionParams = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+};
+const connectDB = async() => {
+  await mongoose.connect(url, connectionParams);
+  console.log("db connected sir");
+};
+module.exports = connectDB;
---
+++ server.js
@@ -0,0 +1,17 @@
+const express  = require ("express");
+const connectDB = require ("./DB/connection");
+const app = express();
+
+connectDB();
+
+
+app.get("/test1", function(req, res){
+  res.send("It is very very very really working dude");
+});
+app.get("/test", function(req, res){
+  res.sendFile(path.join(__dirname, '/pages/test.html'));
+});
+
+const server = app.listen(process.env.PORT || 5000);
+const portNumber = server.address().port;
+console.log("ПОРТ СЕИЧАС ОТКРЫТ "+portNumber);
